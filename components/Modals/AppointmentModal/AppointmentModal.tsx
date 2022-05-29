f8db78ac3b8964817b531791ef9eac364d11d792
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-05-30 00:33:11 +0300

    deleting filed in doctor, profile changes

--- components/Modals/AppointmentModal/AppointmentModal.tsx
+++ components/Modals/AppointmentModal/AppointmentModal.tsx
@@ -1,5 +1,6 @@
 import {addDoc, collection, doc, updateDoc} from "@firebase/firestore";
 import {FC, useState} from "react";
+import useRouter from "next/router";
 import {Button} from "../../ui/Button/Button";
 import {Modal} from "../Modal/Modal";
 import {firestore} from "../../../config/firebase";
@@ -12,6 +13,7 @@ interface Props {
 
 export const AppointmentModal: FC<Props> = ({showModal, setShowModal, specialist}) => {
     const [fullUserName, setFullUserName] = useState('');
+    const router = useRouter;
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [selectValue, setSelectValue] = useState<any>('');
@@ -28,18 +30,13 @@ export const AppointmentModal: FC<Props> = ({showModal, setShowModal, specialist
         });
         await setShowModal(false)
         await updateDoc(docRef, {
-            //date:здесь будет массив который в отдельном методе фильтром убриает использованную дату
+            date: deleteSelectedDate(selectValue)
         })
-        // await deleteDateFromDoctor(selectValue);
+        await router.reload();
     }
-    //
-    // const deleteDateFromDoctor = (date: string) => {
-    //     //удаляет только "локально", уже полученный массив
-    //     const index = specialist.date.indexOf(date);
-    //     if (index > -1) {
-    //         specialist.date.splice(index, 1);
-    //     }
-    // }
+    const deleteSelectedDate = (date: string) => {
+        return specialist.date.filter((e: string) => e !== date)
+    }
 
     return (
         <Modal title={"Запись на прием в клинику"} onClose={() => setShowModal(false)} show={showModal}>
@@ -56,13 +53,13 @@ export const AppointmentModal: FC<Props> = ({showModal, setShowModal, specialist
 
             <select
                 className={"select"}
-                defaultValue={"Выберите дату и время приема"}
+                value={specialist.date.length === 0 ? "К сожалению, запись на данный момент невозможна" : "Выберите дату и время приема"}
                 onChange={(e: any) => setSelectValue(e.target.value)}
             >
                 <option
                     className={"option"}
                     disabled
-                >Выберите дату и время приема
+                >{specialist.date.length === 0 ? "К сожалению, запись на данный момент невозможна" : "Выберите дату и время приема"}
                 </option>
 
                 {specialist.date?.map((date: any) => (
@@ -72,14 +69,15 @@ export const AppointmentModal: FC<Props> = ({showModal, setShowModal, specialist
                         value={date}
                     >{date}</option>
                 ))}
+
             </select>
 
             <Button
                 type="submit"
                 theme="orange"
                 onClick={sendAppointment}
+                disabled={specialist.date.length === 0}
             >Отправить</Button>
-
         </Modal>
     )
 };
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,15 +1,15 @@
 import React, {useEffect, useState} from 'react';
-import {getDocsFromFirebase} from "../utils/getDocsFromFirebase";
-import {useAuth} from "../config/auth";
-import {BaseLayout} from "../components/BaseLayout/BaseLayout";
-import UserCard from "../components/Card/UserCard/UserCard";
-import styles from '/styles/pagesStyles/profile.module.scss';
-import {Button} from '../components/ui/Button/Button';
-import {useRouter} from "next/router";
-import SkeletonAppointmentsComponent from "../components/ui/Skeleton/SkeletonAppointmentsComponent";
 import {addDoc, collection, deleteDoc, doc} from "@firebase/firestore";
 import {firestore} from "../config/firebase";
+import {useAuth} from "../config/auth";
+import {useRouter} from "next/router";
+import {BaseLayout} from "../components/BaseLayout/BaseLayout";
+import UserCard from "../components/Card/UserCard/UserCard";
+import {Button} from '../components/ui/Button/Button';
+import {getDocsFromFirebase} from "../utils/getDocsFromFirebase";
+import SkeletonAppointmentsComponent from "../components/ui/Skeleton/SkeletonAppointmentsComponent";
 import {AddDoctorModal} from "../components/Modals/AddDoctorModal/AddDoctorModal";
+import styles from '/styles/pagesStyles/profile.module.scss';
 
 const Profile = ({usersInfo, appointments, doctorsList}: any) => {
     const currentUser = useAuth();
@@ -82,57 +82,51 @@ const Profile = ({usersInfo, appointments, doctorsList}: any) => {
                 <div className={styles.userInfo}>
                     <UserCard user={user}/>
                     {isAdmin() ?
-                        <>
-                            <div className={styles.doctors}>
-                                <div className={styles.doctors__row}>
-                                    {doctorsList.map((doctor: any) => (
-                                        <div className={styles.doctors__row__item}>
-                                            <p>{doctor.fullName}</p>
-                                            <Button
-                                                type={"button"}
-                                                theme={"transparent"}
-                                                onClick={() => onPreview(doctor)}
-                                                children={"Подробнее"}
-                                            />
-                                            <Button
-                                                type={"button"}
-                                                theme={"transparent"}
-                                                onClick={() => onDelete(doctor.shortName)}
-                                                children={"Удалить"}
-                                            />
+                        <div className={styles.doctors}>
+                            <div className={styles.doctors__row}>
+                                {doctorsList.map((doctor: any) => (
+                                    <div className={styles.doctors__row__item}>
+                                        <p>{doctor.fullName}</p>
+                                        <Button
+                                            type={"button"}
+                                            theme={"transparent"}
+                                            onClick={() => onPreview(doctor)}
+                                            children={"Подробнее"}
+                                        />
+                                        <Button
+                                            type={"button"}
+                                            theme={"transparent"}
+                                            onClick={() => onDelete(doctor.shortName)}
+                                            children={"Удалить"}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                            <Button
+                                type={"button"}
+                                theme={"orange"}
+                                onClick={onAddNew}>
+                                Добавить нового доктора
+                            </Button>
+                            <AddDoctorModal
+                                showModal={showAddDoctorModal}
+                                setShowModal={setShowAddDoctorModal}
+                            />
+                        </div>
+                        :
+                        <div className={styles.visits}>
+                            <h1>Записи на прием</h1>
+                            <div className={styles.visits__info}>
+                                {loading ? <SkeletonAppointmentsComponent/> :
+                                    appointments?.filter((appointment: any) => (currentUser?.email === appointment.email)).map((filteredAppointment: any) => (
+                                        <div key={filteredAppointment.email}
+                                             className={styles.visits__info__content}>
+                                            <h3>Вы записаны к специалисту: {filteredAppointment?.specialist}</h3>
+                                            <h3>Дата и время приема: {filteredAppointment?.date}</h3>
                                         </div>
                                     ))}
-                                </div>
-                                <Button
-                                    type={"button"}
-                                    theme={"orange"}
-                                    onClick={onAddNew}>
-                                    Добавить нового доктора
-                                </Button>
-                                <AddDoctorModal
-                                    showModal={showAddDoctorModal}
-                                    setShowModal={setShowAddDoctorModal}
-                                />
                             </div>
-                        </>
-                        :
-                        <>
-                            <div className={styles.visits}>
-                                <h1>Записи на прием</h1>
-                                {}
-                                <div className={styles.visits__content}>
-                                    {loading ? <SkeletonAppointmentsComponent/> :
-                                        appointments.filter((appointment: any) => (currentUser?.email === appointment.email)).map((filteredAppointment: any) => (
-                                            <div key={filteredAppointment.email}
-                                                 className={styles.visits__content__info}>
-                                                <h3>Вы записаны к специалисту: {filteredAppointment?.specialist}</h3>
-                                                <h3>Дата и время приема: {filteredAppointment?.date}</h3>
-                                            </div>
-                                        ))}
-                                </div>
-                            </div>
-
-                        </>
+                        </div>
                     }
                 </div>
                 <div className={styles.review__block}>
