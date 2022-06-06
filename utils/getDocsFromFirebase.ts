75c6b045dbc2d5998b9269372b416a091bab52e0
Liaisan Akhmetova <akhmetova1423@gmail.com>
2022-06-06 15:12:38 +0300

    done

--- utils/getDocsFromFirebase.ts
+++ utils/getDocsFromFirebase.ts
@@ -3,7 +3,6 @@ import {firestore} from "../config/firebase";
 
 export async function getDocsFromFirebase(path: string) {
     const databaseRef = collection(firestore, path);
-
     const data = await getDocs(databaseRef);
     return data.docs.map((doc) => ({...doc.data()}));
 }
